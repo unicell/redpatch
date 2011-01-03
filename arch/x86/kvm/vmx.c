f9aea926ae387aab0b7206db2db85c3f7da1d31e
Avi Kivity <avi@redhat.com>
2011-01-03 14:28:51 +0200

    KVM: VMX: Save and restore tr selector across mode switches
    When emulating real mode we play with tr hidden state, but leave
    tr.selector alone.  That works well, except for save/restore, since
    loading TR writes it to the hidden state in vmx->rmode.
    
    Fix by also saving and restoring the tr selector; this makes things
    more consistent and allows migration to work during the early
    boot stages of Windows XP.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    (cherry picked from commit d0ba64f9b4b3e41e7b91681fe04a334bc8bfc8f5)
    
    Conflicts:
    
    	arch/x86/kvm/vmx.c

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -1441,6 +1441,7 @@ static void enter_pmode(struct kvm_vcpu *vcpu)
 	vmx->emulation_required = 1;
 	vmx->rmode.vm86_active = 0;
 
+	vmcs_write16(GUEST_TR_SELECTOR, vmx->rmode.tr.selector);
 	vmcs_writel(GUEST_TR_BASE, vmx->rmode.tr.base);
 	vmcs_write32(GUEST_TR_LIMIT, vmx->rmode.tr.limit);
 	vmcs_write32(GUEST_TR_AR_BYTES, vmx->rmode.tr.ar);
@@ -1526,6 +1527,7 @@ static void enter_rmode(struct kvm_vcpu *vcpu)
 		vcpu->srcu_idx = srcu_read_lock(&vcpu->kvm->srcu);
 	}
 
+	vmx->rmode.tr.selector = vmcs_read16(GUEST_TR_SELECTOR);
 	vmx->rmode.tr.base = vmcs_readl(GUEST_TR_BASE);
 	vmcs_writel(GUEST_TR_BASE, rmode_tss_base(vcpu->kvm));
 
