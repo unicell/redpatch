ebda69c8da24c034ca2a8fce479eb2cba3895368
Keegan McAllister <keegan@ksplice.com>
2011-04-25 14:35:55 -0400

    drivers/serial/serial_core.c: prevent reading uninitialized stack memory
    The TIOCGICOUNT device ioctl allows unprivileged users to read
    uninitialized stack memory, because the "reserved" member of the
    serial_icounter_struct struct declared on the stack is not altered or
    zeroed before being copied back to the user.  This patch takes care of
    it.
    
    [ http://lkml.org/lkml/2010/9/15/390 ]
    
    Signed-off-by: Dan Rosenberg <dan.j.rosenberg@gmail.com>

--- drivers/serial/serial_core.c
+++ drivers/serial/serial_core.c
@@ -1090,6 +1090,7 @@ static int uart_get_count(struct uart_state *state,
 	struct uart_icount cnow;
 	struct uart_port *uport = state->uart_port;
 
+	memset(&icount, 0, sizeof(struct serial_icounter_struct));
 	spin_lock_irq(&uport->lock);
 	memcpy(&cnow, &uport->icount, sizeof(struct uart_icount));
 	spin_unlock_irq(&uport->lock);
