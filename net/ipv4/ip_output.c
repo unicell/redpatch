e7659ea34080653baba7dec30c049e065fa8dced
Steffen Klassert <steffen.klassert@secunet.com>
2011-06-29 23:19:32 +0000

    ipv4: Don't use ufo handling on later transformed packets
    We might call ip_ufo_append_data() for packets that will be IPsec
    transformed later. This function should be used just for real
    udp packets. So we check for rt->dst.header_len which is only
    nonzero on IPsec handling and call ip_ufo_append_data() just
    if rt->dst.header_len is zero.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c146066ab80267c3305de5dda6a4083f06df9265)
    
    Conflicts:
    
    	net/ipv4/ip_output.c

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -826,7 +826,7 @@ static int __ip_append_data(struct sock *sk, struct sk_buff_head *queue,
 	cork->length += length;
 	if (((length > mtu) || (skb && skb_is_gso(skb))) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
-	    (rt->u.dst.dev->features & NETIF_F_UFO)) {
+	    (rt->u.dst.dev->features & NETIF_F_UFO) && !rt->u.dst.header_len) {
 		err = ip_ufo_append_data(sk, queue, getfrag, from, length,
 					 hh_len, fragheaderlen, transhdrlen,
 					 mtu, flags);
