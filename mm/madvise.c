3b9e932024cf1701f6e940c49b999519fbe6fc20
Michael Ploujnikov <michael.ploujnikov@oracle.com>
2012-11-07 10:31:09 -0500

    fixup! [mm] Hold a file reference in madvise_remove (Jerome Marchand) [849738 849739] {CVE-2012-3511}

--- mm/madvise.c
+++ mm/madvise.c
@@ -13,7 +13,6 @@
 #include <linux/hugetlb.h>
 #include <linux/sched.h>
 #include <linux/ksm.h>
-#include <linux/fs.h>
 #include <linux/file.h>
 
 /*
@@ -209,24 +208,21 @@ static long madvise_remove(struct vm_area_struct *vma,
 	if ((vma->vm_flags & (VM_SHARED|VM_WRITE)) != (VM_SHARED|VM_WRITE))
 		return -EACCES;
 
-	mapping = vma->vm_file->f_mapping;
+	mapping = f->f_mapping;
 
 	offset = (loff_t)(start - vma->vm_start)
 			+ ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
 	endoff = (loff_t)(end - vma->vm_start - 1)
 			+ ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
 
-	/*
-	 * Filesystem's fallocate may need to take i_mutex.  We need to
-	 * explicitly grab a reference because the vma (and hence the
-	 * vma's reference to the file) can go away as soon as we drop
-	 * mmap_sem.
+	/* vmtruncate_range needs to take i_mutex and i_alloc_sem.
+	 * We need to explicitly grab a reference because the vma (and
+	 * hence the vma's reference to the file) can go away as soon
+	 * as we drop mmap_sem.
 	 */
 	get_file(f);
 	up_read(&current->mm->mmap_sem);
-	error = do_fallocate(f,
-				FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
-				offset, end - start);
+	error = vmtruncate_range(mapping->host, offset, endoff);
 	fput(f);
 	down_read(&current->mm->mmap_sem);
 	return error;
