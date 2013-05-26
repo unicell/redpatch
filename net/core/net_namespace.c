81247062ec439b0a5922e20c700cdead5c69ebb5
unicell <unicell@gmail.com>
2013-05-26 11:06:26 +0800

    Backport 0006-net-Allow-setting-the-network-namespace-by-fd

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -8,6 +8,8 @@
 #include <linux/idr.h>
 #include <linux/rculist.h>
 #include <linux/nsproxy.h>
+#include <linux/proc_fs.h>
+#include <linux/file.h>
 #include <net/net_namespace.h>
 #include <net/netns/generic.h>
 
@@ -223,6 +225,28 @@ struct net *get_net_ns_by_pid(pid_t pid)
 	return net;
 }
 EXPORT_SYMBOL_GPL(get_net_ns_by_pid);
+
+struct net *get_net_ns_by_fd(int fd)
+{
+	struct proc_inode *ei;
+	struct file *file;
+	struct net *net;
+
+	net = ERR_PTR(-EINVAL);
+	file = proc_ns_fget(fd);
+	if (!file)
+		goto out;
+
+	ei = PROC_I(file->f_dentry->d_inode);
+	if (ei->ns_ops != &netns_operations)
+		goto out;
+
+	net = get_net(ei->ns);
+out:
+	if (file)
+		fput(file);
+	return net;
+}
 
 static int __init net_ns_init(void)
 {
